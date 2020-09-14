946a742b509d28e66eef9027bdac542890ea6f97
Itunu Raimi <chavitunu@gmail.com>
2020-09-14 14:01:53 +0100

    starting networkCall

---
+++ Weather App/Weather App/Model/CurrentWeatherData.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherData.swift
+//  Weather App
+//
+//  Created by mac on 14/09/2020.
+//  Copyright © 2020 chavicodes. All rights reserved.
+//
+
+import Foundation
+struct CurrentWeatherData {
+    let main: CurrentWeatherTemperature
+    let weather: [CurrentWeatherType]
+}
